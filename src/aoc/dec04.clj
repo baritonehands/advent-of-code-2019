7fc7d856cfccf50d6aff7c6cf88f7a13eee1940e
Brian Gregg <biscuitalmighty@gmail.com>
2019-12-04 21:10:49 -0600

    Dec04

---
+++ src/aoc/dec04.clj
@@ -0,0 +1,42 @@
+(ns aoc.dec04
+  (:require [aoc.utils :as utils]))
+
+(defn repeats? [digits]
+  (->> (partition-by identity digits)
+       (map count)
+       (some #(> % 1))))
+
+(defn part2-repeats? [digits]
+  (->> (partition-by identity digits)
+       (map count)
+       (some #(= % 2))))
+
+(defn increasing? [digits]
+  (loop [[l r & more] digits]
+    (cond
+      (nil? r) true
+      (> l r) false
+      :else (recur (cons r more)))))
+
+(defn int->digits [n]
+  (mapv #(- (int %) (int \0)) (str n)))
+
+(defn part1-valid? [n]
+  (let [digits (int->digits n)]
+    (and (repeats? digits)
+         (increasing? digits))))
+
+(defn part2-valid? [n]
+  (let [digits (int->digits n)]
+    (and (part2-repeats? digits)
+         (increasing? digits))))
+
+(defn run []
+  (let [input (range 367479 893698)]
+    {:part1 (->> input
+                 (filter part1-valid?)
+                 (count))
+     :part2 (->> input
+                 (filter part2-valid?)
+                 (count))}))
+
